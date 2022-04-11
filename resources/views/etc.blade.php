fb3b7e33f792a9792f1f0bd92e6726c3ccd1dc94
nanana-na <hiubadonakao@icloud.com>
2022-04-11 11:28:37 +0900

    i

--- resources/views/etc.blade.php
+++ resources/views/etc.blade.php
@@ -12,7 +12,7 @@
         <ul>
           <li style="padding-right:20px;"><a class="btn etc-btn btn-hai" href="https://www.sc.admin.saga-u.ac.jp/jikannwari_kyoyo_annai.pdf" target=" _blank"><i class="bi bi-map text-gray" style="padding:7px"></i><br><span class="header-hide header-long">地図</span></a></li>
           <li style="padding-right:20px;"><a class="btn etc-btn btn-hai" href="http://sadai.jp/alumni/dousoukai/song/"><i class="bi bi-music-note-beamed text-gray" style="padding:7px" target=" _blank"></i><br><span class="header-hide">校歌</span></a></li>
-          <li style="padding-right:20px;"><a class="btn etc-btn btn-hai" href="/recommend"><i class="bi bi-music-note-beamed text-gray" style="padding:7px" target=" _blank"></i><br><span class="header-hide">おすすめの設定</span></a></li>
+          <li style="padding-right:20px;"><a class="btn etc-btn btn-hai" href="/recommend"><i class="bi bi-phone" style="padding:7px"></i></i><br><span class="header-hide">おすすめの設定</span></a></li>
         </ul>
       </div>
     </div>
--- resources/views/recommend.blade.php
+++ resources/views/recommend.blade.php
@@ -4,6 +4,10 @@
 
 <div class="container col-md-8 bg-light;">
   <h3 class="h3-blue">おすすめの設定</h3>
-  <p>ホーム画面にて</p>
+  <p>(1)このサイトをSafariで開く</p>
+  <p>(2)画面下部の「共有」アイコンから「ホーム画面に追加」を選択</p>
+  <img src="/images/recommend-1.jpg" alt="" height="400px">
+  <hr>
+  <h4>完了！</h4>
 </div>
 @endsection
