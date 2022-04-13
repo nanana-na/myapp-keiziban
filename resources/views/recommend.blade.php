df4ce4e2768e9dbe937ed10030002ea4f20bfdb4
nanana-na <hiubadonakao@icloud.com>
2022-04-13 15:39:23 +0900

    a

--- resources/views/recommend.blade.php
+++ resources/views/recommend.blade.php
@@ -5,9 +5,10 @@
 <div class="container col-md-8 bg-light;">
   <h3 class="h3-blue">おすすめの設定</h3>
   <p>(1)このサイトをSafariで開く</p>
+  <img style="margin: 5px 0 25px 5px;" src="/images/iphone0.jpg" alt="" width="184px"><br>
   <p>(2)画面下部の「共有」アイコンから「ホーム画面に追加」を選択</p>
-  <img src="/images/iphone1.jpg" alt="" height="400px">
-  <img src="/images/iphone2.jpg" alt="" height="400px">
+  <img style="margin: 5px 0 25px 5px;" src="/images/iphone1.jpg" alt="" height="400px"><br>
+  <img style="margin: 5px 0 25px 5px;" src="/images/iphone2.jpg" alt="" height="400px"><br>
   <hr>
   <h4>完了！</h4>
 </div>
