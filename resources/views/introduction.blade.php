1cd07d9b4214f90b32c1a6f0b09b8a15bd3d110f
nanana-na <hiubadonakao@icloud.com>
2022-04-23 23:51:51 +0900

    a

--- resources/views/introduction.blade.php
+++ resources/views/introduction.blade.php
@@ -13,13 +13,13 @@
   <p>
     授業がきっかけでプログラミングに興味を持ちはじめ何か作ってみたいと思うようになりました。大学2年の冬からPHPとフレームワークのlaravelについて調べ始めました。<br>
     コロナの影響で学生同士が交流する機会が減ってしまったり、孤立化してしまうなどの問題があると感じたので、情報交換ができたり、気軽に交流できたりできるアプリを作りたいと思い制作し始めました。制作中はやり方が分からず躓くことが多々あり時間はかかってしまいましたが、ある程度の所まで完成出来たので公開してみました。基礎知識もほぼ無かったので本業の方が見ると色々と足りないところがあると思いますが、学生の自由研究のつもりで制作されたものとして温かい目で見てください。<br>今回、勉強も兼ねてWebアプリを作ってみましたが少しでも皆さんの助けになれば幸いです。<br>色々失敗しても許して下さい！！<br>
-    あと試しにバイトの掲示もできるようにしたので、「バイトを募集したい！」「広告を載せたい！」とお考えの方は、 <a class="text-info" href="https://twitter.com/sadaikeiziban?ref_src=twsrc%5Etfw" target=" _blank">Twitter</a>か<a class="text-info" href="https://www.instagram.com/sadaikeiziban/" target=" _blank">Instagram</a>にてご連絡くださればお伺いしようと考えています。<br>
+    あと試しにバイトの掲示もできるようにしたので、「バイトを募集したい！」「広告を載せたい！」とお考えの方は、 <a class="text-info" href="https://twitter.com/sadaikeiziban?ref_src=twsrc%5Etfw" target=" _blank">Twitter</a>か<a class="text-info" href="https://www.instagram.com/sadaikeiziban/" target=" _blank">Instagram</a>にてご連絡ください。<br>
   </p>
   <h3 class="h3-blue">
     就職
   </h3>
   <p>
-    SEに興味があるので今後もJava,PHP,(余裕があったらPython)等も勉強していくつもりです。
+    SEに興味があるので今後もGo,PHP,(余裕があったらPython)等も勉強していくつもりです。
   </p>
   <h3 class="h3-blue">最後に</h3>
   <p>
