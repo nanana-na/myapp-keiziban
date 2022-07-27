38d0a7de49815e78e44a34baab46ccbd8b7da3b3
nanana-na <hiubadonakao@icloud.com>
2022-07-27 20:17:28 +0900

    first commit

--- app/Http/Controllers/YetController.php
+++ app/Http/Controllers/YetController.php
@@ -63,8 +63,7 @@ class YetController extends Controller
             return view('/error');
         }
         $yet->save();
-        Mail::to('sadaikeiziban22@gmail.com')->send(new Test('新規登録があります'));
-        return redirect()->route('posts.index')->with('flash_message', '認証されるまでしばらくお待ちください。すぐに確認したい場合はInstagramにご連絡ください');;
+        return redirect()->route('posts.index')->with('flash_message', '認証されるまでしばらくお待ちください。すぐに確認したい場合はInstagramにご連絡ください');
     }
 
     /**
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,42 +16,42 @@ class UsersTableSeeder extends Seeder
         User::create([
             'name' => 'nakao',
             'number' => '20238297',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'nakao1',
             'number' => '20238296',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => '佐賀大学バトミントン部',
             'number' => '0001',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'CUBE',
             'number' => '0002',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'Geese',
             'number' => '0003',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'BDD',
             'number' => '0004',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'CLOVER',
             'number' => '0005',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
         User::create([
             'name' => 'Score!!',
             'number' => '0006',
-            'password' => '$2y$10$.nFpGZITnrSCOgiySsPswuFd9FIr8s6YJ3AvRjCDmaghqSlkel1YC'
+            'password' => '$2y$10$4vwqtdJfNraI/ICszol7YOBOjd1UZOic2YuMsUYBdVR7pCX/9wV.y'
         ]);
     }
 }
